0a953e76b3ae55963228c85366b5846f0764041b
Sebastian Rodrigo <srodrigo@3clics.lat>
2026-02-21 14:31:04 -0300

    cambio traffeik 2

--- astro-app/astro.config.mjs
+++ astro-app/astro.config.mjs
@@ -2,4 +2,10 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
+  vite: {
+    server: {
+      middlewareMode: true,
+      allowedHosts: ['prosal-letras.com', 'localhost', '127.0.0.1'],
+    },
+  },
 });
